38b432f85993986294ff5695aeefdd4e6ff1f1c2
Alvin Yao <ayao00@stuy.edu>
2019-10-29 10:35:54 -0400

    updated names

---
+++ linkedlist.h
@@ -0,0 +1,13 @@
+struct song_node{
+  char name[100];
+  char artist[100];
+  struct song_node *next;
+};
+
+struct node * insert_front(struct song_node * new, char name, char artist);
+struct node * insert_order(struct song_node * new, char name, char artist);
+void print_list(struct song_node * x);
+struct node * find_artist(struct song_node * x, char name, char artist);
+struct node * find_song(struct song_node * x, char artist);
+struct node * random_node(struct song_node * x);
+struct node * node_remove(struct song_node *front, char artist);
