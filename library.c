4f47330de42f5a869fbd74b21b85efd8ae36b703
Alvin Yao <ayao00@stuy.edu>
2019-11-03 20:20:18 -0500

    ALMOST DONE

--- library.c
+++ library.c
@@ -9,8 +9,6 @@ int index_finder(char * artist){
   return *artist - 97;
 }
 
-
-
 void insert_song(struct song_node *table[27], char * artist, char *name){
   int i = index_finder(artist);
   table[i] = insert_order(table[i], artist, name);
@@ -34,7 +32,7 @@ void print_letter(struct song_node * table[27], char * letter){
 
 void print_artist(struct song_node * table[27], char *artist){
   int i = index_finder(artist);
-  struct song_node * art = find_artist(table[i]);
+  struct song_node * art = find_artist(table[i], artist);
   if(strcmp(art ->artist, artist)!=0){
     printf("artist not found");
   }else{
@@ -62,17 +60,17 @@ void print_library(struct song_node * table[27]){
   }
 }
 
-struct song_node * shuffle(struct song_node * table[27]){
+void shuffle(struct song_node * table[27]){
   printf("shuffled playlist\n");
 
   int i = rand();
   i = (int) rand() % 8;
-  for(int j = 0; j < i;i++){
+  for(int j = 0; j < i;){
     int index = rand() % 27;
-    struct song_node * randomsong = random_song(table[indext]);
+    struct song_node * randomsong = random_song(table[index]);
     if(randomsong){
       printf("[%s, %s],", randomsong->artist, randomsong->name);
-      count++;
+      i++;
     }
   }
 }
@@ -80,4 +78,11 @@ struct song_node * shuffle(struct song_node * table[27]){
 struct song_node * remove_libsong(struct song_node *table[27], char * artist, char *name){
   int i = index_finder(artist);
   return remove_song(table[i],artist,name);
+}
+
+void clear_library(struct song_node * table[27]){
+  int i = 0;
+  for(; i< 27;i++){
+    table[i] = NULL;
+  }
 }
